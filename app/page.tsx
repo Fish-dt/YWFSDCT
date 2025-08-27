d14e783115fee363df47b076270bcd6da6670134
fish-dt <fissehadesta70@gmail.com>
2025-08-27 12:49:16 +0300

    v3

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,23 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function DashboardPage() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-background">
       <DashboardHeader />
 
       <div className="flex">
         <main className="flex-1 p-6">
           <div className="space-y-6">
-            <Card className="rounded-xl border-gray-200">
+            <Card className="rounded-xl border-gray-200 dark:border-gray-700">
               <CardHeader>
                 <CardTitle className="text-lg font-medium">Transaction History</CardTitle>
               </CardHeader>
               <CardContent className="p-0">
-                <TransactionTable/>
+                <TransactionTable />
               </CardContent>
             </Card>
           </div>
         </main>
       </div>
     </div>
-  )
+  );
 }
